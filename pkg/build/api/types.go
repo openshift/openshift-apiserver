18b5eeb3ea3d2a34201824ace5a34e32934d2065
gabemontero <gmontero@redhat.com>
2015-07-10 23:27:34 -0400

    issue1875: make the force pull option configurable in the sti build strategy definition; various upstream documentation clarifications / enhancements; comments around reqs for coding/testing changes that run in the builder pod; fixes after merging (changes lost); incorporate comments from Ben

--- pkg/build/api/types.go
+++ pkg/build/api/types.go
@@ -264,6 +264,9 @@ type DockerBuildStrategy struct {
 
 	// Env contains additional environment variables you want to pass into a builder container
 	Env []kapi.EnvVar
+
+	// ForcePull describes if the builder should pull the images from registry prior to building.
+	ForcePull bool
 }
 
 // SourceBuildStrategy defines input parameters specific to an Source build.
@@ -285,6 +288,9 @@ type SourceBuildStrategy struct {
 
 	// Incremental flag forces the Source build to do incremental builds if true.
 	Incremental bool
+
+	// ForcePull describes if the builder should pull the images from registry prior to building.
+	ForcePull bool
 }
 
 // BuildOutput is input to a build strategy and describes the Docker image that the strategy
