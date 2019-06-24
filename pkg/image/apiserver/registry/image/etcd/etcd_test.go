openshift/origin

collapse onto upstream storage RESTOptions

pkg/image/apiserver/registry/image/etcd/etcd_test.go
@@ -16,15 +16,16 @@ import (
 
 	imageapi "github.com/openshift/origin/pkg/image/apis/image"
 	"github.com/openshift/origin/pkg/image/apiserver/registry/image"
-	"github.com/openshift/origin/pkg/util/restoptions"
 	// install all APIs
 	_ "github.com/openshift/origin/pkg/api/install"
+	"k8s.io/apiserver/pkg/registry/generic"
 )
 
 func newStorage(t *testing.T) (*REST, *etcdtesting.EtcdTestServer) {
 	server, etcdStorage := etcdtesting.NewUnsecuredEtcd3TestClientServer(t)
 	etcdStorage.Codec = legacyscheme.Codecs.LegacyCodec(schema.GroupVersion{Group: "image.openshift.io", Version: "v1"})
-	storage, err := NewREST(restoptions.NewSimpleGetter(etcdStorage))
+	restOptions := generic.RESTOptions{StorageConfig: etcdStorage, Decorator: generic.UndecoratedStorage, DeleteCollectionWorkers: 1, ResourcePrefix: "images"}
+	storage, err := NewREST(restOptions)
 	if err != nil {
 		t.Fatal(err)
 	}

openshift/openshift-apiserver

pkg/image/apiserver/registry/image/etcd/etcd_test.go
@@ -9,6 +9,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	apirequest "k8s.io/apiserver/pkg/endpoints/request"
+	"k8s.io/apiserver/pkg/registry/generic"
 	genericregistrytest "k8s.io/apiserver/pkg/registry/generic/testing"
 	"k8s.io/apiserver/pkg/registry/rest"
 	etcdtesting "k8s.io/apiserver/pkg/storage/etcd/testing"
@@ -16,7 +17,6 @@ import (
 
 	imageapi "github.com/openshift/origin/pkg/image/apis/image"
 	"github.com/openshift/origin/pkg/image/apiserver/registry/image"
-	"github.com/openshift/origin/pkg/util/restoptions"
 	// install all APIs
 	_ "github.com/openshift/origin/pkg/api/install"
 )
@@ -24,7 +24,8 @@ import (
 func newStorage(t *testing.T) (*REST, *etcdtesting.EtcdTestServer) {
 	server, etcdStorage := etcdtesting.NewUnsecuredEtcd3TestClientServer(t)
 	etcdStorage.Codec = legacyscheme.Codecs.LegacyCodec(schema.GroupVersion{Group: "image.openshift.io", Version: "v1"})
-	storage, err := NewREST(restoptions.NewSimpleGetter(etcdStorage))
+	imageRESTOptions := generic.RESTOptions{StorageConfig: etcdStorage, Decorator: generic.UndecoratedStorage, DeleteCollectionWorkers: 1, ResourcePrefix: "images"}
+	storage, err := NewREST(imageRESTOptions)
 	if err != nil {
 		t.Fatal(err)
 	}