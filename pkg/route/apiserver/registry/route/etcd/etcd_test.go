c614774dbed4750f0297a9777798a4cbfeae4a05
Michal Fojtik <mfojtik@redhat.com>
2019-09-02 11:16:16 +0200

    BORING: adjust to upstream import moves

--- pkg/route/apiserver/registry/route/etcd/etcd_test.go
+++ pkg/route/apiserver/registry/route/etcd/etcd_test.go
@@ -12,8 +12,7 @@ import (
 	apirequest "k8s.io/apiserver/pkg/endpoints/request"
 	genericregistrytest "k8s.io/apiserver/pkg/registry/generic/testing"
 	"k8s.io/apiserver/pkg/registry/rest"
-
-	// etcdtesting "k8s.io/apiserver/pkg/storage/etcd/testing"
+	 etcdtesting "k8s.io/apiserver/pkg/storage/etcd3/testing"
 	"k8s.io/kubernetes/pkg/api/legacyscheme"
 
 	routeapi "github.com/openshift/openshift-apiserver/pkg/route/apis/route"
