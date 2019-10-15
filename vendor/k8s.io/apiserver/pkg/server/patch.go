899473be9528e7b64062656cde50c1e023da647b
Michal Fojtik <mfojtik@redhat.com>
2019-10-15 15:51:32 +0200

    bump(*): revendor with go mod

---
+++ vendor/k8s.io/apiserver/pkg/server/patch.go
@@ -0,0 +1,8 @@
+package server
+
+func (s *GenericAPIServer) RemoveOpenAPIData() {
+	if s.Handler != nil && s.Handler.NonGoRestfulMux != nil {
+		s.Handler.NonGoRestfulMux.Unregister("/openapi/v2")
+	}
+	s.openAPIConfig = nil
+}
