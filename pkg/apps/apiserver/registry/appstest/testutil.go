bc698a518a627e83630cc7c218344392aadf3473
Michal Fojtik <mfojtik@redhat.com>
2020-08-04 09:37:52 +0200

    BORING: remove cruft in apps test

--- pkg/apps/apiserver/registry/appstest/testutil.go
+++ pkg/apps/apiserver/registry/appstest/testutil.go
@@ -1,21 +1,14 @@
 package appstest
 
 import (
-	"testing"
-
+	appsv1 "github.com/openshift/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/sets"
-	"k8s.io/kubernetes/pkg/api/legacyscheme"
-
-	appsv1 "github.com/openshift/api/apps/v1"
 )
 
 const (
-	ImageStreamName      = "test-image-stream"
-	ImageID              = "0000000000000000000000000000000000000000000000000000000000000001"
-	DockerImageReference = "registry:5000/openshift/test-image-stream@sha256:0000000000000000000000000000000000000000000000000000000000000001"
+	ImageStreamName = "test-image-stream"
 )
 
 func OkDeploymentConfig(version int64) *appsv1.DeploymentConfig {
@@ -49,31 +42,13 @@ func OkDeploymentConfigStatus(version int64) appsv1.DeploymentConfigStatus {
 	}
 }
 
-func OkImageChangeDetails() *appsv1.DeploymentDetails {
-	return &appsv1.DeploymentDetails{
-		Causes: []appsv1.DeploymentCause{{
-			Type: appsv1.DeploymentTriggerOnImageChange,
-			ImageTrigger: &appsv1.DeploymentCauseImageTrigger{
-				From: corev1.ObjectReference{
-					Name: ImageStreamName + ":latest",
-					Kind: "ImageStreamTag",
-				}}}}}
-}
-
-func OkConfigChangeDetails() *appsv1.DeploymentDetails {
-	return &appsv1.DeploymentDetails{
-		Causes: []appsv1.DeploymentCause{{
-			Type: appsv1.DeploymentTriggerOnConfigChange,
-		}}}
-}
-
 func OkStrategy() appsv1.DeploymentStrategy {
 	return appsv1.DeploymentStrategy{
 		Type: appsv1.DeploymentStrategyTypeRecreate,
 		Resources: corev1.ResourceRequirements{
 			Limits: corev1.ResourceList{
-				corev1.ResourceName(corev1.ResourceCPU):    resource.MustParse("10"),
-				corev1.ResourceName(corev1.ResourceMemory): resource.MustParse("10G"),
+				corev1.ResourceCPU:    resource.MustParse("10"),
+				corev1.ResourceMemory: resource.MustParse("10G"),
 			},
 		},
 		RecreateParams: &appsv1.RecreateDeploymentStrategyParams{
@@ -83,52 +58,9 @@ func OkStrategy() appsv1.DeploymentStrategy {
 	}
 }
 
-func OkCustomStrategy() appsv1.DeploymentStrategy {
-	return appsv1.DeploymentStrategy{
-		Type:         appsv1.DeploymentStrategyTypeCustom,
-		CustomParams: OkCustomParams(),
-		Resources: corev1.ResourceRequirements{
-			Limits: corev1.ResourceList{
-				corev1.ResourceName(corev1.ResourceCPU):    resource.MustParse("10"),
-				corev1.ResourceName(corev1.ResourceMemory): resource.MustParse("10G"),
-			},
-		},
-	}
-}
-
-func OkCustomParams() *appsv1.CustomDeploymentStrategyParams {
-	return &appsv1.CustomDeploymentStrategyParams{
-		Image: "openshift/origin-deployer",
-		Environment: []corev1.EnvVar{
-			{
-				Name:  "ENV1",
-				Value: "VAL1",
-			},
-		},
-		Command: []string{"/bin/echo", "hello", "world"},
-	}
-}
-
 func mkintp(i int) *int64 {
 	v := int64(i)
 	return &v
-}
-
-func OkRollingStrategy() appsv1.DeploymentStrategy {
-	return appsv1.DeploymentStrategy{
-		Type: appsv1.DeploymentStrategyTypeRolling,
-		RollingParams: &appsv1.RollingDeploymentStrategyParams{
-			UpdatePeriodSeconds: mkintp(1),
-			IntervalSeconds:     mkintp(1),
-			TimeoutSeconds:      mkintp(20),
-		},
-		Resources: corev1.ResourceRequirements{
-			Limits: corev1.ResourceList{
-				corev1.ResourceName(corev1.ResourceCPU):    resource.MustParse("10"),
-				corev1.ResourceName(corev1.ResourceMemory): resource.MustParse("10G"),
-			},
-		},
-	}
 }
 
 func OkSelector() map[string]string {
@@ -173,24 +105,6 @@ func OkPodTemplate() *corev1.PodTemplateSpec {
 	}
 }
 
-func OkPodTemplateChanged() *corev1.PodTemplateSpec {
-	template := OkPodTemplate()
-	template.Spec.Containers[0].Image = DockerImageReference
-	return template
-}
-
-func OkPodTemplateMissingImage(missing ...string) *corev1.PodTemplateSpec {
-	set := sets.NewString(missing...)
-	template := OkPodTemplate()
-	for i, c := range template.Spec.Containers {
-		if set.Has(c.Name) {
-			// remember that slices use copies, so have to ref array entry explicitly
-			template.Spec.Containers[i].Image = ""
-		}
-	}
-	return template
-}
-
 func OkConfigChangeTrigger() appsv1.DeploymentTriggerPolicy {
 	return appsv1.DeploymentTriggerPolicy{
 		Type: appsv1.DeploymentTriggerOnConfigChange,
@@ -211,58 +125,4 @@ func OkImageChangeTrigger() appsv1.DeploymentTriggerPolicy {
 			},
 		},
 	}
-}
-
-func OkTriggeredImageChange() appsv1.DeploymentTriggerPolicy {
-	ict := OkImageChangeTrigger()
-	ict.ImageChangeParams.LastTriggeredImage = DockerImageReference
-	return ict
-}
-
-func OkNonAutomaticICT() appsv1.DeploymentTriggerPolicy {
-	ict := OkImageChangeTrigger()
-	ict.ImageChangeParams.Automatic = false
-	return ict
-}
-
-func OkTriggeredNonAutomatic() appsv1.DeploymentTriggerPolicy {
-	ict := OkNonAutomaticICT()
-	ict.ImageChangeParams.LastTriggeredImage = DockerImageReference
-	return ict
-}
-
-func TestDeploymentConfig(config *appsv1.DeploymentConfig) *appsv1.DeploymentConfig {
-	config.Spec.Test = true
-	return config
-}
-
-func RemoveTriggerTypes(config *appsv1.DeploymentConfig, triggerTypes ...appsv1.DeploymentTriggerType) {
-	types := sets.NewString()
-	for _, triggerType := range triggerTypes {
-		types.Insert(string(triggerType))
-	}
-
-	remaining := []appsv1.DeploymentTriggerPolicy{}
-	for _, trigger := range config.Spec.Triggers {
-		if types.Has(string(trigger.Type)) {
-			continue
-		}
-		remaining = append(remaining, trigger)
-	}
-
-	config.Spec.Triggers = remaining
-}
-
-func RoundTripConfig(t *testing.T, config *appsv1.DeploymentConfig) *appsv1.DeploymentConfig {
-	versioned, err := legacyscheme.Scheme.ConvertToVersion(config, appsv1.SchemeGroupVersion)
-	if err != nil {
-		t.Errorf("unexpected conversion error: %v", err)
-		return nil
-	}
-	defaulted, err := legacyscheme.Scheme.ConvertToVersion(versioned, appsv1.SchemeGroupVersion)
-	if err != nil {
-		t.Errorf("unexpected conversion error: %v", err)
-		return nil
-	}
-	return defaulted.(*appsv1.DeploymentConfig)
 }
