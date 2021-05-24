7d5621dc1296d6811faeebfdfb338cd766631616
Miciah Masters <miciah.masters@gmail.com>
2021-05-24 10:05:34 -0400

    Revert "Bug 1896977: Enhance API host name validation"
    This reverts commit 402c0fdc3b10abfef4d3b4fbc09e14e33a0a8dbf.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1957584

--- pkg/route/apiserver/registry/route/etcd/etcd_test.go
+++ pkg/route/apiserver/registry/route/etcd/etcd_test.go
@@ -95,7 +95,7 @@ func TestCreate(t *testing.T) {
 }
 
 func TestCreateWithAllocation(t *testing.T) {
-	allocator := &testAllocator{Hostname: "bar.com"}
+	allocator := &testAllocator{Hostname: "bar"}
 	storage, server := newStorage(t, allocator)
 	defer server.Terminate(t)
 	defer storage.Store.DestroyFunc()
@@ -105,7 +105,7 @@ func TestCreateWithAllocation(t *testing.T) {
 		t.Fatalf("unable to create object: %v", err)
 	}
 	result := obj.(*routeapi.Route)
-	if result.Spec.Host != "bar.com" {
+	if result.Spec.Host != "bar" {
 		t.Fatalf("unexpected route: %#v", result)
 	}
 	if v, ok := result.Annotations[route.HostGeneratedAnnotationKey]; !ok || v != "true" {
