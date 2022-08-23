73a32a101e5f5c8d46149c90054a6589e4eed4ac
Abu Kashem <akashem@redhat.com>
2022-08-23 08:12:38 -0400

    fix printer panic

--- pkg/security/printers/internalversion/printer_test.go
+++ pkg/security/printers/internalversion/printer_test.go
@@ -1,23 +1,63 @@
 package internalversion
 
 import (
+	"fmt"
+	"runtime"
 	"testing"
 
-	securityapi "github.com/openshift/openshift-apiserver/pkg/security/apis/security"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kprinters "k8s.io/kubernetes/pkg/printers"
+
+	"github.com/openshift/openshift-apiserver/pkg/security/apis/security"
 )
 
-func TestPrintSecurityContextConstraintWithDeepCopy(t *testing.T) {
-	scc := securityapi.SecurityContextConstraints{}
-	rows, err := printSecurityContextConstraint(&scc, kprinters.GenerateOptions{})
-
-	if err != nil {
-		t.Fatalf("expected no error, but got: %#v", err)
+func TestPrintersWithDeepCopy(t *testing.T) {
+	tests := []struct {
+		name    string
+		printer func() ([]metav1.TableRow, error)
+	}{
+		{
+			name: "RangeAllocation",
+			printer: func() ([]metav1.TableRow, error) {
+				return printRangeAllocation(&security.RangeAllocation{}, kprinters.GenerateOptions{})
+			},
+		},
+		{
+			name: "SecurityContextConstraints",
+			printer: func() ([]metav1.TableRow, error) {
+				return printSecurityContextConstraint(&security.SecurityContextConstraints{}, kprinters.GenerateOptions{})
+			},
+		},
 	}
-	if len(rows) <= 0 {
-		t.Fatalf("expected to have at least one TableRow, but got: %d", len(rows))
-	}
 
-	// should not panic
-	rows[0].DeepCopy()
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			rows, err := test.printer()
+			if err != nil {
+				t.Fatalf("expected no error, but got: %#v", err)
+			}
+			if len(rows) <= 0 {
+				t.Fatalf("expected to have at least one TableRow, but got: %d", len(rows))
+			}
+
+			func() {
+				defer func() {
+					if err := recover(); err != nil {
+						// Same as stdlib http server code. Manually allocate stack
+						// trace buffer size to prevent excessively large logs
+						const size = 64 << 10
+						buf := make([]byte, size)
+						buf = buf[:runtime.Stack(buf, false)]
+						err = fmt.Errorf("%q stack:\n%s", err, buf)
+
+						t.Errorf("Expected no panic, but got: %v", err)
+					}
+				}()
+
+				// should not panic
+				rows[0].DeepCopy()
+			}()
+
+		})
+	}
 }
