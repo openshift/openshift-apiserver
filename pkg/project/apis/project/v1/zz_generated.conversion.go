b035979340dc8f2826d69837f2bf36787b7b26f1
wangke19 <kewang@redhat.com>
2025-05-21 14:32:24 +0800

    Runs make verify to regenerate the needed code

--- pkg/project/apis/project/v1/zz_generated.conversion.go
+++ pkg/project/apis/project/v1/zz_generated.conversion.go
@@ -8,7 +8,7 @@ package v1
 import (
 	unsafe "unsafe"
 
-	v1 "github.com/openshift/api/project/v1"
+	projectv1 "github.com/openshift/api/project/v1"
 	project "github.com/openshift/openshift-apiserver/pkg/project/apis/project"
 	corev1 "k8s.io/api/core/v1"
 	conversion "k8s.io/apimachinery/pkg/conversion"
@@ -24,60 +24,60 @@ func init() {
 // RegisterConversions adds conversion functions to the given scheme.
 // Public to allow building arbitrary schemes.
 func RegisterConversions(s *runtime.Scheme) error {
-	if err := s.AddGeneratedConversionFunc((*v1.Project)(nil), (*project.Project)(nil), func(a, b interface{}, scope conversion.Scope) error {
-		return Convert_v1_Project_To_project_Project(a.(*v1.Project), b.(*project.Project), scope)
+	if err := s.AddGeneratedConversionFunc((*projectv1.Project)(nil), (*project.Project)(nil), func(a, b interface{}, scope conversion.Scope) error {
+		return Convert_v1_Project_To_project_Project(a.(*projectv1.Project), b.(*project.Project), scope)
 	}); err != nil {
 		return err
 	}
-	if err := s.AddGeneratedConversionFunc((*project.Project)(nil), (*v1.Project)(nil), func(a, b interface{}, scope conversion.Scope) error {
-		return Convert_project_Project_To_v1_Project(a.(*project.Project), b.(*v1.Project), scope)
+	if err := s.AddGeneratedConversionFunc((*project.Project)(nil), (*projectv1.Project)(nil), func(a, b interface{}, scope conversion.Scope) error {
+		return Convert_project_Project_To_v1_Project(a.(*project.Project), b.(*projectv1.Project), scope)
 	}); err != nil {
 		return err
 	}
-	if err := s.AddGeneratedConversionFunc((*v1.ProjectList)(nil), (*project.ProjectList)(nil), func(a, b interface{}, scope conversion.Scope) error {
-		return Convert_v1_ProjectList_To_project_ProjectList(a.(*v1.ProjectList), b.(*project.ProjectList), scope)
+	if err := s.AddGeneratedConversionFunc((*projectv1.ProjectList)(nil), (*project.ProjectList)(nil), func(a, b interface{}, scope conversion.Scope) error {
+		return Convert_v1_ProjectList_To_project_ProjectList(a.(*projectv1.ProjectList), b.(*project.ProjectList), scope)
 	}); err != nil {
 		return err
 	}
-	if err := s.AddGeneratedConversionFunc((*project.ProjectList)(nil), (*v1.ProjectList)(nil), func(a, b interface{}, scope conversion.Scope) error {
-		return Convert_project_ProjectList_To_v1_ProjectList(a.(*project.ProjectList), b.(*v1.ProjectList), scope)
+	if err := s.AddGeneratedConversionFunc((*project.ProjectList)(nil), (*projectv1.ProjectList)(nil), func(a, b interface{}, scope conversion.Scope) error {
+		return Convert_project_ProjectList_To_v1_ProjectList(a.(*project.ProjectList), b.(*projectv1.ProjectList), scope)
 	}); err != nil {
 		return err
 	}
-	if err := s.AddGeneratedConversionFunc((*v1.ProjectRequest)(nil), (*project.ProjectRequest)(nil), func(a, b interface{}, scope conversion.Scope) error {
-		return Convert_v1_ProjectRequest_To_project_ProjectRequest(a.(*v1.ProjectRequest), b.(*project.ProjectRequest), scope)
+	if err := s.AddGeneratedConversionFunc((*projectv1.ProjectRequest)(nil), (*project.ProjectRequest)(nil), func(a, b interface{}, scope conversion.Scope) error {
+		return Convert_v1_ProjectRequest_To_project_ProjectRequest(a.(*projectv1.ProjectRequest), b.(*project.ProjectRequest), scope)
 	}); err != nil {
 		return err
 	}
-	if err := s.AddGeneratedConversionFunc((*project.ProjectRequest)(nil), (*v1.ProjectRequest)(nil), func(a, b interface{}, scope conversion.Scope) error {
-		return Convert_project_ProjectRequest_To_v1_ProjectRequest(a.(*project.ProjectRequest), b.(*v1.ProjectRequest), scope)
+	if err := s.AddGeneratedConversionFunc((*project.ProjectRequest)(nil), (*projectv1.ProjectRequest)(nil), func(a, b interface{}, scope conversion.Scope) error {
+		return Convert_project_ProjectRequest_To_v1_ProjectRequest(a.(*project.ProjectRequest), b.(*projectv1.ProjectRequest), scope)
 	}); err != nil {
 		return err
 	}
-	if err := s.AddGeneratedConversionFunc((*v1.ProjectSpec)(nil), (*project.ProjectSpec)(nil), func(a, b interface{}, scope conversion.Scope) error {
-		return Convert_v1_ProjectSpec_To_project_ProjectSpec(a.(*v1.ProjectSpec), b.(*project.ProjectSpec), scope)
+	if err := s.AddGeneratedConversionFunc((*projectv1.ProjectSpec)(nil), (*project.ProjectSpec)(nil), func(a, b interface{}, scope conversion.Scope) error {
+		return Convert_v1_ProjectSpec_To_project_ProjectSpec(a.(*projectv1.ProjectSpec), b.(*project.ProjectSpec), scope)
 	}); err != nil {
 		return err
 	}
-	if err := s.AddGeneratedConversionFunc((*project.ProjectSpec)(nil), (*v1.ProjectSpec)(nil), func(a, b interface{}, scope conversion.Scope) error {
-		return Convert_project_ProjectSpec_To_v1_ProjectSpec(a.(*project.ProjectSpec), b.(*v1.ProjectSpec), scope)
+	if err := s.AddGeneratedConversionFunc((*project.ProjectSpec)(nil), (*projectv1.ProjectSpec)(nil), func(a, b interface{}, scope conversion.Scope) error {
+		return Convert_project_ProjectSpec_To_v1_ProjectSpec(a.(*project.ProjectSpec), b.(*projectv1.ProjectSpec), scope)
 	}); err != nil {
 		return err
 	}
-	if err := s.AddGeneratedConversionFunc((*v1.ProjectStatus)(nil), (*project.ProjectStatus)(nil), func(a, b interface{}, scope conversion.Scope) error {
-		return Convert_v1_ProjectStatus_To_project_ProjectStatus(a.(*v1.ProjectStatus), b.(*project.ProjectStatus), scope)
+	if err := s.AddGeneratedConversionFunc((*projectv1.ProjectStatus)(nil), (*project.ProjectStatus)(nil), func(a, b interface{}, scope conversion.Scope) error {
+		return Convert_v1_ProjectStatus_To_project_ProjectStatus(a.(*projectv1.ProjectStatus), b.(*project.ProjectStatus), scope)
 	}); err != nil {
 		return err
 	}
-	if err := s.AddGeneratedConversionFunc((*project.ProjectStatus)(nil), (*v1.ProjectStatus)(nil), func(a, b interface{}, scope conversion.Scope) error {
-		return Convert_project_ProjectStatus_To_v1_ProjectStatus(a.(*project.ProjectStatus), b.(*v1.ProjectStatus), scope)
+	if err := s.AddGeneratedConversionFunc((*project.ProjectStatus)(nil), (*projectv1.ProjectStatus)(nil), func(a, b interface{}, scope conversion.Scope) error {
+		return Convert_project_ProjectStatus_To_v1_ProjectStatus(a.(*project.ProjectStatus), b.(*projectv1.ProjectStatus), scope)
 	}); err != nil {
 		return err
 	}
 	return nil
 }
 
-func autoConvert_v1_Project_To_project_Project(in *v1.Project, out *project.Project, s conversion.Scope) error {
+func autoConvert_v1_Project_To_project_Project(in *projectv1.Project, out *project.Project, s conversion.Scope) error {
 	out.ObjectMeta = in.ObjectMeta
 	if err := Convert_v1_ProjectSpec_To_project_ProjectSpec(&in.Spec, &out.Spec, s); err != nil {
 		return err
@@ -89,11 +89,11 @@ func autoConvert_v1_Project_To_project_Project(in *v1.Project, out *project.Proj
 }
 
 // Convert_v1_Project_To_project_Project is an autogenerated conversion function.
-func Convert_v1_Project_To_project_Project(in *v1.Project, out *project.Project, s conversion.Scope) error {
+func Convert_v1_Project_To_project_Project(in *projectv1.Project, out *project.Project, s conversion.Scope) error {
 	return autoConvert_v1_Project_To_project_Project(in, out, s)
 }
 
-func autoConvert_project_Project_To_v1_Project(in *project.Project, out *v1.Project, s conversion.Scope) error {
+func autoConvert_project_Project_To_v1_Project(in *project.Project, out *projectv1.Project, s conversion.Scope) error {
 	out.ObjectMeta = in.ObjectMeta
 	if err := Convert_project_ProjectSpec_To_v1_ProjectSpec(&in.Spec, &out.Spec, s); err != nil {
 		return err
@@ -105,11 +105,11 @@ func autoConvert_project_Project_To_v1_Project(in *project.Project, out *v1.Proj
 }
 
 // Convert_project_Project_To_v1_Project is an autogenerated conversion function.
-func Convert_project_Project_To_v1_Project(in *project.Project, out *v1.Project, s conversion.Scope) error {
+func Convert_project_Project_To_v1_Project(in *project.Project, out *projectv1.Project, s conversion.Scope) error {
 	return autoConvert_project_Project_To_v1_Project(in, out, s)
 }
 
-func autoConvert_v1_ProjectList_To_project_ProjectList(in *v1.ProjectList, out *project.ProjectList, s conversion.Scope) error {
+func autoConvert_v1_ProjectList_To_project_ProjectList(in *projectv1.ProjectList, out *project.ProjectList, s conversion.Scope) error {
 	out.ListMeta = in.ListMeta
 	if in.Items != nil {
 		in, out := &in.Items, &out.Items
@@ -126,15 +126,15 @@ func autoConvert_v1_ProjectList_To_project_ProjectList(in *v1.ProjectList, out *
 }
 
 // Convert_v1_ProjectList_To_project_ProjectList is an autogenerated conversion function.
-func Convert_v1_ProjectList_To_project_ProjectList(in *v1.ProjectList, out *project.ProjectList, s conversion.Scope) error {
+func Convert_v1_ProjectList_To_project_ProjectList(in *projectv1.ProjectList, out *project.ProjectList, s conversion.Scope) error {
 	return autoConvert_v1_ProjectList_To_project_ProjectList(in, out, s)
 }
 
-func autoConvert_project_ProjectList_To_v1_ProjectList(in *project.ProjectList, out *v1.ProjectList, s conversion.Scope) error {
+func autoConvert_project_ProjectList_To_v1_ProjectList(in *project.ProjectList, out *projectv1.ProjectList, s conversion.Scope) error {
 	out.ListMeta = in.ListMeta
 	if in.Items != nil {
 		in, out := &in.Items, &out.Items
-		*out = make([]v1.Project, len(*in))
+		*out = make([]projectv1.Project, len(*in))
 		for i := range *in {
 			if err := Convert_project_Project_To_v1_Project(&(*in)[i], &(*out)[i], s); err != nil {
 				return err
@@ -147,11 +147,11 @@ func autoConvert_project_ProjectList_To_v1_ProjectList(in *project.ProjectList, 
 }
 
 // Convert_project_ProjectList_To_v1_ProjectList is an autogenerated conversion function.
-func Convert_project_ProjectList_To_v1_ProjectList(in *project.ProjectList, out *v1.ProjectList, s conversion.Scope) error {
+func Convert_project_ProjectList_To_v1_ProjectList(in *project.ProjectList, out *projectv1.ProjectList, s conversion.Scope) error {
 	return autoConvert_project_ProjectList_To_v1_ProjectList(in, out, s)
 }
 
-func autoConvert_v1_ProjectRequest_To_project_ProjectRequest(in *v1.ProjectRequest, out *project.ProjectRequest, s conversion.Scope) error {
+func autoConvert_v1_ProjectRequest_To_project_ProjectRequest(in *projectv1.ProjectRequest, out *project.ProjectRequest, s conversion.Scope) error {
 	out.ObjectMeta = in.ObjectMeta
 	out.DisplayName = in.DisplayName
 	out.Description = in.Description
@@ -159,11 +159,11 @@ func autoConvert_v1_ProjectRequest_To_project_ProjectRequest(in *v1.ProjectReque
 }
 
 // Convert_v1_ProjectRequest_To_project_ProjectRequest is an autogenerated conversion function.
-func Convert_v1_ProjectRequest_To_project_ProjectRequest(in *v1.ProjectRequest, out *project.ProjectRequest, s conversion.Scope) error {
+func Convert_v1_ProjectRequest_To_project_ProjectRequest(in *projectv1.ProjectRequest, out *project.ProjectRequest, s conversion.Scope) error {
 	return autoConvert_v1_ProjectRequest_To_project_ProjectRequest(in, out, s)
 }
 
-func autoConvert_project_ProjectRequest_To_v1_ProjectRequest(in *project.ProjectRequest, out *v1.ProjectRequest, s conversion.Scope) error {
+func autoConvert_project_ProjectRequest_To_v1_ProjectRequest(in *project.ProjectRequest, out *projectv1.ProjectRequest, s conversion.Scope) error {
 	out.ObjectMeta = in.ObjectMeta
 	out.DisplayName = in.DisplayName
 	out.Description = in.Description
@@ -171,31 +171,31 @@ func autoConvert_project_ProjectRequest_To_v1_ProjectRequest(in *project.Project
 }
 
 // Convert_project_ProjectRequest_To_v1_ProjectRequest is an autogenerated conversion function.
-func Convert_project_ProjectRequest_To_v1_ProjectRequest(in *project.ProjectRequest, out *v1.ProjectRequest, s conversion.Scope) error {
+func Convert_project_ProjectRequest_To_v1_ProjectRequest(in *project.ProjectRequest, out *projectv1.ProjectRequest, s conversion.Scope) error {
 	return autoConvert_project_ProjectRequest_To_v1_ProjectRequest(in, out, s)
 }
 
-func autoConvert_v1_ProjectSpec_To_project_ProjectSpec(in *v1.ProjectSpec, out *project.ProjectSpec, s conversion.Scope) error {
+func autoConvert_v1_ProjectSpec_To_project_ProjectSpec(in *projectv1.ProjectSpec, out *project.ProjectSpec, s conversion.Scope) error {
 	out.Finalizers = *(*[]core.FinalizerName)(unsafe.Pointer(&in.Finalizers))
 	return nil
 }
 
 // Convert_v1_ProjectSpec_To_project_ProjectSpec is an autogenerated conversion function.
-func Convert_v1_ProjectSpec_To_project_ProjectSpec(in *v1.ProjectSpec, out *project.ProjectSpec, s conversion.Scope) error {
+func Convert_v1_ProjectSpec_To_project_ProjectSpec(in *projectv1.ProjectSpec, out *project.ProjectSpec, s conversion.Scope) error {
 	return autoConvert_v1_ProjectSpec_To_project_ProjectSpec(in, out, s)
 }
 
-func autoConvert_project_ProjectSpec_To_v1_ProjectSpec(in *project.ProjectSpec, out *v1.ProjectSpec, s conversion.Scope) error {
+func autoConvert_project_ProjectSpec_To_v1_ProjectSpec(in *project.ProjectSpec, out *projectv1.ProjectSpec, s conversion.Scope) error {
 	out.Finalizers = *(*[]corev1.FinalizerName)(unsafe.Pointer(&in.Finalizers))
 	return nil
 }
 
 // Convert_project_ProjectSpec_To_v1_ProjectSpec is an autogenerated conversion function.
-func Convert_project_ProjectSpec_To_v1_ProjectSpec(in *project.ProjectSpec, out *v1.ProjectSpec, s conversion.Scope) error {
+func Convert_project_ProjectSpec_To_v1_ProjectSpec(in *project.ProjectSpec, out *projectv1.ProjectSpec, s conversion.Scope) error {
 	return autoConvert_project_ProjectSpec_To_v1_ProjectSpec(in, out, s)
 }
 
-func autoConvert_v1_ProjectStatus_To_project_ProjectStatus(in *v1.ProjectStatus, out *project.ProjectStatus, s conversion.Scope) error {
+func autoConvert_v1_ProjectStatus_To_project_ProjectStatus(in *projectv1.ProjectStatus, out *project.ProjectStatus, s conversion.Scope) error {
 	out.Phase = core.NamespacePhase(in.Phase)
 	if in.Conditions != nil {
 		in, out := &in.Conditions, &out.Conditions
@@ -212,11 +212,11 @@ func autoConvert_v1_ProjectStatus_To_project_ProjectStatus(in *v1.ProjectStatus,
 }
 
 // Convert_v1_ProjectStatus_To_project_ProjectStatus is an autogenerated conversion function.
-func Convert_v1_ProjectStatus_To_project_ProjectStatus(in *v1.ProjectStatus, out *project.ProjectStatus, s conversion.Scope) error {
+func Convert_v1_ProjectStatus_To_project_ProjectStatus(in *projectv1.ProjectStatus, out *project.ProjectStatus, s conversion.Scope) error {
 	return autoConvert_v1_ProjectStatus_To_project_ProjectStatus(in, out, s)
 }
 
-func autoConvert_project_ProjectStatus_To_v1_ProjectStatus(in *project.ProjectStatus, out *v1.ProjectStatus, s conversion.Scope) error {
+func autoConvert_project_ProjectStatus_To_v1_ProjectStatus(in *project.ProjectStatus, out *projectv1.ProjectStatus, s conversion.Scope) error {
 	out.Phase = corev1.NamespacePhase(in.Phase)
 	if in.Conditions != nil {
 		in, out := &in.Conditions, &out.Conditions
@@ -233,6 +233,6 @@ func autoConvert_project_ProjectStatus_To_v1_ProjectStatus(in *project.ProjectSt
 }
 
 // Convert_project_ProjectStatus_To_v1_ProjectStatus is an autogenerated conversion function.
-func Convert_project_ProjectStatus_To_v1_ProjectStatus(in *project.ProjectStatus, out *v1.ProjectStatus, s conversion.Scope) error {
+func Convert_project_ProjectStatus_To_v1_ProjectStatus(in *project.ProjectStatus, out *projectv1.ProjectStatus, s conversion.Scope) error {
 	return autoConvert_project_ProjectStatus_To_v1_ProjectStatus(in, out, s)
 }
