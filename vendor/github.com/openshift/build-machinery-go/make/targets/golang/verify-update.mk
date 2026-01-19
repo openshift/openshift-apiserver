5db8cc7af459ade51dfcdbc4079ff8fdc6eaea5c
Bryce Palmer <bpalmer@redhat.com>
2026-01-19 16:04:43 -0500

    fixup! fix vendoring issues causing build failures
    Signed-off-by: Bryce Palmer <bpalmer@redhat.com>

--- vendor/github.com/openshift/build-machinery-go/make/targets/golang/verify-update.mk
+++ vendor/github.com/openshift/build-machinery-go/make/targets/golang/verify-update.mk
@@ -3,12 +3,11 @@ include $(addprefix $(dir $(lastword $(MAKEFILE_LIST))), \
 )
 
 go_files_count :=$(words $(GO_FILES))
-chunk_size :=1000
 
 verify-gofmt:
 	$(info Running `$(GOFMT) $(GOFMT_FLAGS)` on $(go_files_count) file(s).)
 	@TMP=$$( mktemp ); \
-	find . -name '*.go' -not -path '*/vendor/*' -not -path '*/_output/*' -print | xargs -n $(chunk_size) $(GOFMT) $(GOFMT_FLAGS) | tee $${TMP}; \
+	$(GOFMT) $(GOFMT_FLAGS) $(GO_FILES) | tee $${TMP}; \
 	if [ -s $${TMP} ]; then \
 		echo "$@ failed - please run \`make update-gofmt\`"; \
 		exit 1; \
@@ -17,7 +16,7 @@ verify-gofmt:
 
 update-gofmt:
 	$(info Running `$(GOFMT) $(GOFMT_FLAGS) -w` on $(go_files_count) file(s).)
-	@find . -name '*.go' -not -path '*/vendor/*' -not -path '*/_output/*' -print | xargs -n $(chunk_size) $(GOFMT) $(GOFMT_FLAGS) -w
+	@$(GOFMT) $(GOFMT_FLAGS) -w $(GO_FILES)
 .PHONY: update-gofmt
 
 
