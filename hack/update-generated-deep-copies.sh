bd13696e107aa0fd2b2caaa83c429476a9bc982f
Jacob See <5027680+jacobsee@users.noreply.github.com>
2026-07-21 15:06:17 -0700

    deepcopy-gen no longer accepts bounding-dirs

--- hack/update-generated-deep-copies.sh
+++ hack/update-generated-deep-copies.sh
@@ -25,4 +25,4 @@ ALL_FQ_APIS=(
 )
 
 echo "Generating deepcopy funcs"
-${GOPATH}/bin/deepcopy-gen "${ALL_FQ_APIS[@]}" --output-file zz_generated.deepcopy.go --bounding-dirs $(codegen::join , "${ALL_FQ_APIS[@]}") --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.txt "$@"
+"${GOPATH}/bin/deepcopy-gen" "${ALL_FQ_APIS[@]}" --output-file zz_generated.deepcopy.go --go-header-file "${SCRIPT_ROOT}/hack/boilerplate.txt" "$@"
